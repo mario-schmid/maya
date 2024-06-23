978eebc1e7a2227aeee2617a209685590215ed4a
mario <mario-schmid@mein.gmx>
2024-06-23 14:38:56 +0200

    update lists

--- lib/helper/maya_lists.dart
+++ lib/helper/maya_lists.dart
@@ -1,41 +1,43 @@
 class MayaLists {
   final List<String> strTone = [
-    'Jun',
-    'Keb',
-    'Oxib',
-    'Kajib',
-    'Job',
-    'Waqib',
-    'Wuqub',
-    'Wajxaqib',
-    'Belejeb',
-    'Lajuj',
-    'Julajuj',
-    'Kablajuj',
-    'Oxlajuj'
+    // own      // alicia     // maya tecum
+    'Jun',      // Jun        // Jun
+    'Keb',      // Keb'       // Kab'
+    'Oxib',     // Oxib'      // Oxib'
+    'Kajib',    // Kajib'     // Kiejeb'
+    'Job',      // Job'       // Job'
+    'Waqib',    // Waqib'     // Waqib'
+    'Wuqub',    // Wuqub'     // Wukub'
+    'Wajxaqib', // Wajxaqib'  // Waqxaquib'
+    'Belejeb',  // B'elejeb'  // B'elejeb'
+    'Lajuj',    // Lajuj      // Lajuj
+    'Julajuj',  // Ju'lajuj   // Junlajuj
+    'Kablajuj', // Kab'lajuj  // Kab'lajuj
+    'Oxlajuj'   // Oxlajuj    // Oxlajuj
   ];
 
   final List<String> strNahual = [
-    'Imox',
-    'Iq',
-    'Aqabal',
-    'Kat',
-    'Kan',
-    'Kame',
-    'Kej',
-    'Qanil',
-    'Toj',
-    'Tzi',
-    'Batz',
-    'E',
-    'Aj',
-    'Ix',
-    'Tzikin',
-    'Ajmaq',
-    'Noj',
-    'Tijax',
-    'Kawoq',
-    'Ajpu'
+    // own    // alicia     // maya tecum
+    'Imox',   // Imox       // Imox
+    'Iq',     // Iq'        // Iq'
+    'Aqabal', // Aq'ab'al   // Ak'ab'al
+    'Kat',    // K'at       // K'at
+    'Kan',    // Kan        // Kan
+    'Kame',                 // Keme
+    'Kej',                  // Kiej
+    'Qanil',                // Q'anil
+    'Toj',                  // Toj
+    'Tzi',                  // Tz'i
+    'Batz',                 // B'atz'
+    'E',                    // E
+    'Aj',     // Aj         // Aj
+    'Ix',     // I'x        // I'x
+    'Tzikin', // Tz'ikin    // Tz'ikin
+    'Ajmaq',  // Ajmaq      // Ajmaq
+    'Noj',    // No'j       // N'oj
+    'Tijax',  // Tijax      // Tijax
+    'Kawoq',  // Kawoq      // Kawok
+    'Ajpu'    // Ajpu       // Ajpu
   ];
 
   final List<String> strWinal = [
