4cc4478aae3595d619fe9e2f0633bf590d008693
mario <mario-schmid@mein.gmx>
2024-10-06 20:27:25 +0200

    adjustments

--- android/app/src/main/kotlin/com/software/maya/MainActivity.kt
+++ android/app/src/main/kotlin/com/software/maya/MainActivity.kt
@@ -1,15 +1,5 @@
 package com.software.maya
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-import org.devio.flutter.splashscreen.SplashScreen
-
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // SplashScreen.show(this) 
-        // or enable full screen
-        SplashScreen.show(this, true)
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity : FlutterActivity() {}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,11 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_core
-import firebase_messaging
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
